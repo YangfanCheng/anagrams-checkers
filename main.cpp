e9657c88f0859ec2013291320ffb2700afcd016c
YangfanCheng <syhhsyhk13@gmail.com>
2015-08-10 21:41:11 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+bool anagrams(string str1, string str2);
+
+int main(){
+    string str1="abcd";
+    string str2="dcba";
+    
+    cout<<anagrams(str1,str2)<<endl;
+    system("pause");
+    return 0;    
+}
+
+bool anagrams(string str1, string str2){
+    if(str1.length()!=str2.length()) return false;
+    for(int i=0;i <str1.length(); i++){
+        for(int j=0;j<str2.length();j++){
+            if(str1[i]==str2[j]){
+                str1[i]='*';
+                break;
+            }
+        }    
+    }
+    for(int i=0;i<str1.length();i++){
+        if(str1[i]!='*') return false;
+    }
+    return true;
+}
